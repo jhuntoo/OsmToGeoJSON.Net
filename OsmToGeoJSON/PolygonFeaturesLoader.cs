89a3496a3a94f406176cde87926d75f15bc1b345
Jonathon Lee <jonnolee84@gmail.com>
2014-12-08 16:55:49 -0800

    Added Supporting OSM types

---
+++ OsmToGeoJSON/PolygonFeaturesLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace OsmToGeoJSON
+{
+    public class PolygonFeaturesLoader : IPolygonFeaturesLoader
+    {
+        public Dictionary<string, object> Load()
+        {
+            return JsonConvert.DeserializeObject<Dictionary<string, object>>
+                (File.ReadAllText("polygonFeatures.json"), new JsonConverter[] { new PolygonFeatureDictionaryConverter() });
+        }
+    }
+}
